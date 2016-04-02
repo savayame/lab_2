21e0dccf430c97c758fd7ae01846fc2d92494941
savayame <statyanant7@ya.ru>
2016-04-02 10:49:58 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,25 @@
 #include<stdio.h>
-#include< math.h>
+#include<math.h>
 int main()
 {
-	int x, y, a, b, c, d;
+	double x, y, a, b, c, d;
 	printf("x=");
-	scanf("%d",&x);
+	scanf("%lf",&x);
 	printf("a=");
-	scanf("%d",&a);
+	scanf("%lf",&a);
 	printf("b=");
-    scanf("%d",&b);
+	scanf("%lf",&b);
 	printf("c=");
-    scanf("%d",&c);
-    y=a*x^2+b*x+c;
+	scanf("%lf",&c);
+	y=a*x^2+b*x+c;
 	d=b^2-4*a*c;
 	if (d==0)
-		printf("x=%d",-b/(2*a));
+		printf("x=%lf",-b/(2*a));
 	if (d>0)
 	{
-		 printf("x1=%d\n",(-b+sqrt(d))/2*a);
-         printf("x2=%d",(-b+sqrt(d))/2*a);
-    }
+		 printf("x1=%lf\n",(-b+sqrt(d))/2*a);
+		 printf("x2=%lf",(-b+sqrt(d))/2*a);
+	}
 	else
 		printf("d<0");
 	return 0;
